12eded8a3d62179de7658b964faf15ac8d9f2e6c
NikitaBelyakov <nikita.belyakov@gomobile.ru>
2021-05-30 14:54:04 +0300

    docs: documented db class

--- src/resources/db/memory.db.js
+++ src/resources/db/memory.db.js
@@ -1,20 +1,46 @@
+/**
+ * Memory databse.
+ * @class
+ */
 class MemoryDB {
+    /**
+     * Create DB instance and create store.
+     * @constructor
+     */
     constructor() {
         this.STORE = []
     }
 
+    /**
+     * Return all items from store.
+     * @returns {any[]} 
+     */
     getAll() {
         return this.STORE;
     }
 
+    /**
+     * Get item by id param.
+     * @param {string} id item Id.
+     * @returns {any}
+     */
     getById(id) {
         return this.STORE.find((item) => item.id === id) || null;
     }
 
+    /**
+     * Push item to store.
+     * @param {any} item 
+     */
     addItem(item) {
         this.STORE.push(item);
     }
 
+    /**
+     * Remove item by id param.
+     * @param {string} id 
+     * @returns 
+     */
     deleteItemById(id) {
         const item = this.getById(id);
         if(!item) return false;
@@ -22,6 +48,11 @@ class MemoryDB {
         return item;
     }
 
+    /**
+     * Remove item by custom prop.
+     * @param {string} prop
+     * @param {string} id
+     */
     deleteByProp(prop, value) {
         this.STORE = this.STORE
             .filter(item => item[prop] !== value)
