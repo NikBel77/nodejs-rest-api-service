eca6ddb73df9f72f8a9990664882690c66795ea4
NikitaBelyakov <nikita.belyakov@gomobile.ru>
2021-07-11 10:15:21 +0300

    feat: add exeption filter;

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './exeption/http-exeption.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(4000);
 }
 bootstrap();
